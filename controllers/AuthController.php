d308a6ace6f8277d7626dc6b77333f293c3248ac
IsabelleNascimento25 <noisabelle06@gmail.com>
2024-09-11 10:14:39 -0300

    codigo

---
+++ controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+//Requer arquivo "user.php" que contem o model usar com as funcóes para manipulação de dados de usuários
+require_once 'models/user.php';
+
+class AuthController
+{
+    // Cria função responsável pelo processo de login
+    public function login()
+    {
+        
+    }
+}
+?>
---
+++ routes.php
@@ -0,0 +1,26 @@
+<?php
+//inclui arquivos de controlador para lidar com diferentes açóes 
+
+require 'controllers/AuthController.php';
+//inclui o controlador de autenticação
+require 'controllers/UserController.php';
+//inclui o controlador de usuário
+require 'controllers/DashboardController.php';
+//inclui o controlador de dashboard
+
+//Cria instânciais dos controladores para utilizar seus métodos 
+$authController = new AuthController(); 
+//Instância 
+
+//Coleta a ação da URL se não houver definida, usa "login por padrão"
+
+$action = $_GET ['action'] ?? 'login'; //Usa operador de coalescência nula (??) para definir 'login' se 'action não estiver presente'
+
+switch($action){
+    case 'login':
+        //Verifica a ação solicitada e chama o método apropriado do controlador
+        $authController -> login();//Chama o método login do controlador de autenticação
+        
+}
+
+?>
