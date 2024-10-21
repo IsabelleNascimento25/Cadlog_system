b36b2c036dc23c1b86bed2234b8f6dceb4dbfbc1
IsabelleNascimento25 <noisabelle06@gmail.com>
2024-10-21 08:02:54 -0300

    modificando rotas e manipulações de usuarios

--- routes.php
+++ routes.php
@@ -1,38 +1,30 @@
 <?php
-//inclui arquivos de controlador para lidar com diferentes açóes 
+//Inclui arquivos de controlador para lidar com diferentes ações como, listar usuário cadastrados, cadastrar os usuários, etc.
+require 'controllers/AuthController.php'; //Inclui o controlador de autenticação
+require 'controllers/UserController.php'; //Inclui o controlador de usuário
+require 'controllers/DashboardController.php'; //Inclui o controlador de dashboard
 
-require 'controllers/AuthController.php';
-//inclui o controlador de autenticação
-require 'controllers/UserController.php';
-//inclui o controlador de usuário
-require 'controllers/DashboardController.php';
-//inclui o controlador de dashboard
+//Cria instâncias dos controladores para utilizar os métodos
+$authController = new AuthController(); //Instancia o controlador de autenticação
+$userController = new UserController();
+$dashboardController = new DashboardController();
 
-//Cria instânciais dos controladores para utilizar seus métodos 
-$authController = new AuthController(); 
-$userController = new UserController(); 
+//Coleta a ação da url, se não houver ação definida, usa login por padrão
+$action = $_GET['action'] ?? 'login'; //Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
 
-//Instância 
-
-//Coleta a ação da URL se não houver definida, usa "login por padrão"
-
-$action = $_GET ['action'] ?? 'login'; //Usa operador de coalescência nula (??) para definir 'login' se 'action não estiver presente'
-
+//Verifica a ação solicitada e chama o método apropriado do controlador
 switch($action){
     case 'login':
-        //Verifica a ação solicitada e chama o método apropriado do controlador
-        $authController -> login();//Chama o método login do controlador de autenticação
-        break; 
-    case 'register':
+        $authController->login(); //Chama o método de login do controlador de autenticação
+        break;
+        case 'register':
             $userController->register();
             break;
-
-    case 'dashboard': 
-        $dash
-    default: 
-    $authController ->login();
-    break;
-        
+            case 'logout':
+                $authController ->logout();
+                break;
+        default:
+        $authController->login();
+        break;
 }
-
 ?>
